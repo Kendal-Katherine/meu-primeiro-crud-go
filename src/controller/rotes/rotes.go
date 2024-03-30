507d4e3ea786d6f95b05509d0684be0c555e4c90
Kendal Katherine <kendal.katherine3@gmail.com>
2024-03-30 16:37:15 -0300

    Criando rotas da aplicação

---
+++ src/controller/rotes/rotes.go
@@ -0,0 +1,15 @@
+package rotes
+
+import (
+	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+
+	r.GET("/getUserById/:userId", controller.FindUserById)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userId", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userId", controller.DeleteUser)
+}
