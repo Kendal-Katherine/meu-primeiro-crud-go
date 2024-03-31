450c80d46c83fc9fc796e60ff29c6704619c0ea6
Kendal Katherine <kendal.katherine3@gmail.com>
2024-03-31 01:45:11 -0300

    Adicionando logs ao projeto

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,25 +1,43 @@
 package controller
 
 import (
-	"fmt"
+	"net/http"
 
-	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/configurantion/rest_err"
+	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/configurantion/logger"
+	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/configurantion/validation"
 	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/controller/model/request"
+	"github.com/Kendal-Katherine/meu-primeiro-crud-go/src/controller/model/response"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap/zapcore"
 )
 
 func CreateUser(c *gin.Context) {
+	logger.Info("Init CreateUser controller",
+		zapcore.Field{
+			Key:    "Journey",
+			String: "createUser",
+		},
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds, erro=%s\n", err.Error()))
+		logger.Error("Error trying to validate info", err)
+		restErr := validation.ValidateUserError(err)
 
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 
-	fmt.Println(userRequest)
-
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
+	logger.Info("User created sucessfully",
+		zapcore.Field{
+			Key:    "Journey",
+			String: "createUser",
+		})
+	c.JSON(http.StatusOK, response)
 }
